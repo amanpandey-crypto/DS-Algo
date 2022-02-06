ecc15b3c17e08a0234b8ecadb3877753eac62677
aman <pandeyaman879@gmail.com>
2022-02-06 23:46:03 +0530

    Level order traversal

---
+++ 104. Maximum Depth of Binary Tree.cpp
@@ -0,0 +1,45 @@
+/*
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along 
+the longest path from the root node down to the farthest leaf node.
+*/
+
+#include<iostream>
+#include<bits/stdc++.h>
+#include<climits>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        int depth = 0;
+        
+        if (root == NULL) return depth;
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while (!q.empty()) {
+            int size = q.size();
+            depth++;
+            for (int i = 0; i < size; i++) {
+                TreeNode* temp = q.front();
+                q.pop();
+                if (temp -> left != NULL) q.push(temp -> left);
+                if (temp -> right != NULL) q.push(temp -> right);
+            }
+        }
+        
+        return depth;
+    }
+};
